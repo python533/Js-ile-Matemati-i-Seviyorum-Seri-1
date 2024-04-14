4667001c8f263ae6614455097b43796417b0f048
python533 <143389231+python533@users.noreply.github.com>
2024-04-15 00:28:32 +0300

    Js ile Matematiği Seviyorum
    Dizide verili Nesnelerin Hangisi ? cm ile ölçülebilir

---
+++ matemtaik.js
@@ -0,0 +1,9 @@
+const nesneler=['Zürafa','Ağaç','Elma'];
+
+for (var i = 0; i < nesneler.length; i++) {
+if ([i] == 0,393700787) {
+console.log("Nesneler Cm ile ölçülebilir.")
+}else {
+console.console.log("Nesneler Cm ile Ölçülemez");
+}
+}
